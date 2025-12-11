197e9c093b5e8d75034424e81d58ca44789851ad
Shrinivas <jshrinivas1996@gmail.com>
2025-12-11 23:53:42 +0530

    problems on filter method

---
+++ JS Practice/Filter/startWithLetterA.js
@@ -0,0 +1,7 @@
+// Filter strings that start with the letter “A”
+
+let arr = ["Ajay", "Sahil", "Argon", "Ash", "Misti"];
+
+let filteredArray = arr.filter((item) => item[0] === "A");
+
+console.log(filteredArray);
