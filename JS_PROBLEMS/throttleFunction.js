dd061fbf046bcc8abb25beb87c7c8857ca62e879
Shrinivas <jshrinivas1996@gmail.com>
2025-12-24 23:21:30 +0530

    Throttle Function

---
+++ JS_PROBLEMS/throttleFunction.js
@@ -0,0 +1,15 @@
+// Throttle Function
+function throttle(fn, limit) {
+  let inThrottle = false;
+
+  return function (...args) {
+    if (!inThrottle) {
+      fn.apply(this, args);
+      inThrottle = true;
+
+      setTimeout(() => {
+        inThrottle = false;
+      }, limit);
+    }
+  };
+}
