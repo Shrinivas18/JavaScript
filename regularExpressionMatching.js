ddab1c7000f5a697e9f8d80a33328850797eb498
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2026-01-29 23:34:30 +0530

    Regular Expression Matching

---
+++ regularExpressionMatching.js
@@ -0,0 +1,39 @@
+/*
+💡Regular Expression Matching
+
+Implement regex matching with . and *.
+
+Input: s = "aab", p = "c*a*b"
+Output: true
+
+💡 Idea (Dynamic Programming)
+dp[i][j] → does s[0..i) match p[0..j)
+* = zero or more of previous character
+
+*/
+
+var isMatch = function(s, p) {
+    const dp = Array(s.length + 1)
+        .fill(null)
+        .map(() => Array(p.length + 1).fill(false));
+
+    dp[0][0] = true;
+
+    for (let j = 2; j <= p.length; j++) {
+        if (p[j - 1] === '*') dp[0][j] = dp[0][j - 2];
+    }
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 1; j <= p.length; j++) {
+            if (p[j - 1] === '.' || p[j - 1] === s[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === '*') {
+                dp[i][j] =
+                    dp[i][j - 2] ||
+                    ((p[j - 2] === '.' || p[j - 2] === s[i - 1]) &&
+                     dp[i - 1][j]);
+            }
+        }
+    }
+    return dp[s.length][p.length];
+};
