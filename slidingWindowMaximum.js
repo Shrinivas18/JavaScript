1017984460e5cb0b1f91e487a743e1272e28f2de
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2026-01-30 23:22:34 +0530

    Sliding Window Maximum

---
+++ slidingWindowMaximum.js
@@ -0,0 +1,37 @@
+/*
+Sliding Window Maximum
+
+*🧠 Problem
+
+Find the maximum in every window of size k.
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+
+💡 Idea (Monotonic Deque)
+ - Keep deque decreasing
+ - Front always has max
+
+*/
+
+var maxSlidingWindow = function(nums, k) {
+    const deque = [];
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        while (deque.length && deque[0] <= i - k) {
+            deque.shift();
+        }
+
+        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+    return result;
+};
