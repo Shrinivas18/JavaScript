4494f80be9805f0874fa4bb672503a05187a1873
Shrinivas <jshrinivas1996@gmail.com>
2026-02-01 21:23:09 +0530

    updating folder structure

--- JS_PROBLEMS/firstNonRepeatingCharacters.js
+++ JS_PROBLEMS/firstNonRepeatingCharacters.js
@@ -1,14 +1,25 @@
-// Find the first non-repeating character in a string.
+// First Non-Repeating Character
+/*
+Problem Statement
 
-function firstNonRepeatingCharacter(str) {
+Write a function that takes a string and returns the first character that does not repeat anywhere in the string.
+ - If all characters repeat, return null
+ - The function should be case-sensitive
+ - Time complexity should be O(n)
+*/
+
+function firstUniqueChar(str) {
   const count = {};
+
   for (let char of str) {
     count[char] = (count[char] || 0) + 1;
   }
+
   for (let char of str) {
     if (count[char] === 1) {
       return char;
     }
   }
+
   return null;
 }
